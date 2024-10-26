c1d1f63a6b4ee08594bfd24fe5e9f1f655ac4fbe
M Daniyal SoftWare Engineer [React ,WordPress, PHP] <61905200+daniyal-sudo@users.noreply.github.com>
2024-10-26 18:07:19 +1100

    Update Registration.jsx

--- src/Component/Registration/Registration.jsx
+++ src/Component/Registration/Registration.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-import "./registration.css";
+import "./Registration.css";
 import ImageUploader from "../CommonComponent/ImgUploader";
 
 const Registration = () => {
