cd21c27b25543503e543f542472187bcb287d2fc
hamidRazach <150281909+HamidRazach@users.noreply.github.com>
2024-12-03 01:07:18 +0500

    icon css

--- src/Component/Home/Sidebar.jsx
+++ src/Component/Home/Sidebar.jsx
@@ -70,12 +70,44 @@ const Sidebar = () => {
         //   background:'white'
         // }}
       >
-        <div className="text-center">
+        <div className="text-center d-flex flex-row align-items-center justify-content-between">
           <img
             src="/image 45.png"
             alt="logo"
             className="img-fluid side_logo_height_width"
           />
+<button
+className="hamid-icon"
+      type="button"
+      aria-label="Close"
+      style={{
+        background: 'none',
+        border: 'none',
+        padding: 0,
+        cursor: 'pointer',
+        position: 'relative',
+        right: '-12px',
+        top: '-12px',
+        display: "none"
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="30"
+        fill="currentColor"
+        className="bi bi-x"
+        viewBox="0 0 16 16"
+        stroke="currentColor"
+        strokeWidth="1"
+      >
+    <path
+      d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+    />
+  </svg>
+</button>
+
+
         </div>
 
         <div className="d-flex flex-column align-items-center mt-5">
